7922734dcbdf8a07d2a932600f021ab35d154486
Patrick Quinn-Graham <make-contact@pftqg.com>
2020-01-04 18:42:47 +1100

    Create NoSmartcardRequired group & automatically add local admins to it

--- system-setup/configure-ad/configure-ad.sh
+++ system-setup/configure-ad/configure-ad.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-dsconfigad -mobile enable -mobileconfirm disable -shell /bin/zsh -groups TWOPATS\Mac-LocalAdmin
+dsconfigad -mobile enable \
+  -mobileconfirm disable \
+  -shell /bin/zsh \
+  -groups TWOPATS\Mac-LocalAdmin
--- system-setup/smartcard-login/smartcard-login-setup.sh
+++ system-setup/smartcard-login/smartcard-login-setup.sh
@@ -1,4 +1,45 @@
 #!/bin/sh
 
+log() {
+  echo $(date) "${@}"
+}
+
+GROUP_NAME=NoSmartcardRequired
+GROUP_PATH=/Groups/${GROUP_NAME}
+
 # Create NoSmartcardRequired group
-dscl . create /Groups/NoSmartcardRequired
+log Creating ${GROUP_PATH}.
+dscl . create ${GROUP_PATH}
+
+dscl . create ${GROUP_PATH} RealName "No Smartcard Required"
+dscl . create ${GROUP_PATH} passwd "*"
+dscl . create ${GROUP_PATH} gid 799
+
+dscl . list /Users | grep -v '^_.*\|daemon\|root\|nobody' | while read localUser
+do
+  # check if user is in the local admin group
+  IsLocalAdmin=$(dseditgroup -o checkmember -m $localUser admin | awk '{print $1}')
+
+  IsLocalAccount=no
+  dscl . read "/Users/${localUser}" OriginalAuthenticationAuthority | grep OriginalAuthenticationAuthority &>/dev/null
+  if [ $? -ne 0 ]; then
+    IsLocalAccount=yes
+  fi
+
+	IsSmartCardDisabled=$(dseditgroup -o checkmember -m $localUser ${GROUP_NAME} | awk '{print $1}')
+
+  if [[ "$IsLocalAccount" = "yes" ]]; then
+    if [[ "$IsLocalAdmin" = "yes" ]]; then
+      if [[ "$IsSmartCardDisabled" = "no" ]]; then
+        log "${localUser}: Add to ${GROUP_NAME}"
+        /usr/sbin/dseditgroup -o edit -a "$localUser" -t user "${GROUP_NAME}"
+      else
+        log "${localUser}: Already in ${GROUP_NAME}"
+      fi
+    else
+      log "${localUser}: Is not an admin, ignoring"
+    fi
+  else
+    log "${localUser}: User is not a local account."
+  fi
+done
